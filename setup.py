8fd32100854168b644a1767405f6b4ad699fe6ac
Jairo <jairo.alves@petrobras.com>
2020-07-30 10:49:09 -0300

    V1.0
    simplificada a estrutura

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
    name='valuation',
-   version='1.0',
+   version='0.1.0',
    description='Simple Valuation Module',
    author='Jairo Alves',
    author_email='jairo.luciano@gmail.com',
-   packages=['valuation'],
+   packages=find_packages("valuation"),
    install_requires=['pandas', 'numpy', 'matplotlib', 'seaborn', 'IPython'],
 )
